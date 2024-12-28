bb81ce79b44c314b6450c71b457a8e85bc4b81cc
ayhan168585 <ayhan168585@hotmail.com>
2024-12-28 20:30:03 +0300

    appmodule oluşturacak şekilde proe tekrar oluşturuldu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Northwind';
+  product={productId:1,productName:"Bardak",categoryId:2,unitPrice:10}
+  product2={productId:2,productName:"Laptop",categoryId:2,unitPrice:10}
+  product3={productId:3,productName:"Mouse",categoryId:2,unitPrice:10}
+  product4={productId:4,productName:"Keyboard",categoryId:2,unitPrice:10}
+  product5={productId:5,productName:"Camera",categoryId:2,unitPrice:10}
+  
+  user="Ayhan Özer"
+  products=[this.product,this.product2,this.product3,this.product4,this.product5]
 }
