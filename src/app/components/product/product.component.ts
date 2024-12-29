c0f4fb89ecc4ebd6788e00d0659e12a8adfef1d9
ayhan168585 <ayhan168585@hotmail.com>
2024-12-29 21:21:36 +0300

    apiden gelen veriyi modelledik

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
   standalone: false,
-  
+
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
-  
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  product={productId:1,productName:"Bardak",categoryId:2,unitPrice:10}
-  product2={productId:2,productName:"Laptop",categoryId:2,unitPrice:10}
-  product3={productId:3,productName:"Mouse",categoryId:2,unitPrice:10}
-  product4={productId:4,productName:"Keyboard",categoryId:2,unitPrice:10}
-  product5={productId:5,productName:"Camera",categoryId:2,unitPrice:10}
-
-  products=[this.product,this.product2,this.product3,this.product4,this.product5] 
-
+  products: Product[] = [
+   
+  ];
 }
-
