d072dfba493726144039f41e9e36e08b2f39e3d6
ayhan168585 <ayhan168585@hotmail.com>
2025-01-03 02:10:40 +0300

    seçilen kategoriye göre ürün getirme işlemi yapıldı.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -13,10 +14,16 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded=false
  
-  constructor(private productService:ProductService) {}
+  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-   this.getProducts()
+  this.activatedRoute.params.subscribe(params=>{
+    if(params["categoryId"]){
+      this.getProductsByCategory(params["categoryId"])
+    }else{
+      this.getProducts()
+    }
+  })
   }
 
   getProducts() {
@@ -25,5 +32,11 @@ export class ProductComponent implements OnInit {
     this.dataLoaded=true
 
    })
+  }
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true
+    })
   }
 }
